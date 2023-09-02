0b87e3dea4f36dded649c2f1b1bab98defb1cba3
NachiketSaraf <sarafnachiket@gmail.com>
2023-09-02 11:56:04 +0530

    test

--- src/test/java/com/bootcoding/junit/array/ArraySumTest.java
+++ src/test/java/com/bootcoding/junit/array/ArraySumTest.java
@@ -2,6 +2,8 @@ package com.bootcoding.junit.array;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class ArraySumTest {
@@ -20,5 +22,21 @@ public class ArraySumTest {
         int actualSum = arraySum.getSum(arr);
         int expectedSum = 15;
         assertEquals(expectedSum,actualSum);
+    }
+    @Test
+    public void testMax(){
+        ArraySum arraySum = new ArraySum();
+        int[] arr = {1,2,3,4,5};
+        int actualSum = arraySum.getMax(arr);
+        int expectedSum = 5;
+        assertEquals(expectedSum,actualSum);
+    }
+    @Test
+    public void testEven(){
+        ArraySum arraySum = new ArraySum();
+        int[] arr = {1,2,3,4,5};
+        ArrayList<Integer> actualSum = arraySum.getEven(arr);
+        int[] expectedSum = {2,4};
+        assertArrayEquals(expectedSum,actualSum);
     }
 }
