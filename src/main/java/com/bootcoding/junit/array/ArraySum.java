de12731d4214c9e2ce98af356989d9ef6d2bdc83
NachiketSaraf <sarafnachiket@gmail.com>
2023-09-02 11:56:27 +0530

    Array different methods

--- src/main/java/com/bootcoding/junit/array/ArraySum.java
+++ src/main/java/com/bootcoding/junit/array/ArraySum.java
@@ -1,5 +1,7 @@
 package com.bootcoding.junit.array;
 
+import java.util.ArrayList;
+
 public class ArraySum {
     public int getSum(int[] num){
         int sum = 0;
@@ -8,4 +10,20 @@ public class ArraySum {
         }
         return sum;
     }
+    public int getMax(int[] num){
+        int max = Integer.MIN_VALUE;
+        for (int n :num) {
+            max=  Math.max(n,max);
+        }
+        return max;
+    }
+    public ArrayList<Integer> getEven(int[] num){
+        ArrayList<Integer> even = new ArrayList<>();
+        for (int n :num) {
+            if (n%2==0) even.add(n);
+        }
+        return even ;
+    }
+
+
 }
