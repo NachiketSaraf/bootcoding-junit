ab5fe4599aa7d87dab486bab6942eed9d4df8c84
NachiketSaraf <sarafnachiket@gmail.com>
2023-09-13 02:03:45 +0530

    Array operations

--- src/main/java/com/bootcoding/junit/array/ArrayOperations.java
+++ src/main/java/com/bootcoding/junit/array/ArrayOperations.java
@@ -4,21 +4,21 @@ import java.util.ArrayList;
 
 public class ArrayOperations {
 
-    public int getSum(int[] num){
+    public static int getSum(int[] num){
         int sum = 0;
         for (int n : num) {
             sum += n;
         }
         return sum;
     }
-    public int getMax(int[] num){
+    public static int getMax(int[] num){
         int max = Integer.MIN_VALUE;
         for (int n :num) {
             max=  Math.max(n,max);
         }
         return max;
     }
-    public ArrayList<Integer> getEven(int[] num){
+    public static ArrayList<Integer> getEven(int[] num){
         ArrayList<Integer> even = new ArrayList<>();
         for (int n :num) {
             if (n%2==0) even.add(n);
